8945f1065179107fd5e67fe4ac8ecbf4b1ab9613
Luc Peeters <l.m.p.peeters@tilburguniversity.edu>
2022-03-22 00:51:55 +0100

    update project analysis

--- ODCM_project_analysis.R
+++ ODCM_project_analysis.R
@@ -16,18 +16,22 @@ library(data.table)
 library(stringr)
 
 # Import data
-setwd("C:/Users/Gebruiker/Documents/0. Master Marketing Analytics/Skills Online Data Coll. & Mgt")
-df <- read.csv("testing5.csv")
+setwd("C:/Users/Gebruiker/Documents/0. Master Marketing Analytics/Skills Online Data Coll. & Mgt/ODCM/data")
+df <- read.csv("mediamarkt_scraper_output.csv")
 View(df)
 
 
-                                           ####################
-                                           ##data-preparation##
-                                           ####################
+                                                  ####################
+                                                  ##data-preparation##
+                                                  ####################
 
+# Remove superfluous columns
+df <- df %>% select(-Beeldschermdiagonaal..cm.inch..)
 
-# Remove ',-' from 'price' variable
-df$price <- (gsub(",-", "", df$price))
+# Remove inconvenient text 
+df$price <- (gsub(",-", "", df$price))                                                # ',-' from 'price' variable
+df$Beeldschermdiagonaal..cm.. <- (gsub("cm", "", df$Beeldschermdiagonaal..cm..))      # 'cm' from 'Beeldschermdiagonaal..cm..' variable
+df$Beeldschermdiagonaal..inch.. <- (gsub("inch", "", df$Beeldschermdiagonaal..inch..))  # 'inch' from 'Beeldschermdiagonaal..inch..' variable
 
 # Assure numeric datatypes
 column_list_numeric <- grep('price|rating|nr_reviews', colnames(df,), value=T)
@@ -39,6 +43,7 @@ for (column in column_list_numeric){
 df$device_type <- as.factor(df$device_type)
 df$instock <- as.factor(df$instock)
 
+
 # Remove 'Beamers & projectie' value of 'device_type'
 df <- subset(df, device_type != "Beamers & projectie")
 
@@ -47,10 +52,7 @@ df$device_type <- (gsub("XIAOMI 12 256GB - Grijs|XIAOMI POCO X4 Pro 5G 256GB - Z
                                           "Mobiele telefoons", df$device_type))
 
 # Add column consisting of brand names
-df$brand <-word(unlist(df$name[1:1598]))
-
-
-
+df$brand <-word(unlist(df$name[1:1580]))
 
                                                 ####################
                                                 ###data-analysis####
@@ -72,7 +74,6 @@ ggplot(df, aes(x = brand)) +
   geom_bar() + coord_flip()
 
 # distribution of device types 
-                                                                                                                     
 plotdata_devices <- df %>%
   count(device_type)                                                            
 View(plotdata_devices)                                                      
@@ -80,36 +81,51 @@ View(plotdata_devices)
 ggplot(plotdata_devices, 
        aes(x = reorder(device_type, n),                                     
            y = n)) + 
-  geom_bar(stat = "identity", fill = "indianred3", color = "black") +                     # total units per device type                     
+  geom_bar(stat = "identity", fill = "indianred3", color = "black") +                    # total units per device type                     
   geom_text(aes(label = n), vjust = 2, size = 4) +
   labs(x = "Device type", 
        y = "Frequency", 
        title  = "Distribution of device types")
 
+# means and standard deviations per device type
+mean_prices <- df %>% group_by(device_type) %>%         
+  summarise(mean_price = mean(price, na.rm = TRUE))                                      # mean price
+sd_prices <- df %>% group_by(device_type) %>% 
+  summarise(sd_prices = sd(price, na.rm = TRUE))                                         # standard deviation price
 
-mean_prices <- df %>% group_by(device_type) %>% 
-  summarise(mean_price = mean(price, na.rm = TRUE))
-                                                                                          # mean prices per device type
+mean_rating <- df %>% group_by(device_type) %>%         
+  summarise(mean_rating = mean(rating, na.rm = TRUE))                                    # mean rating
+sd_rating <- df %>% group_by(device_type) %>% 
+  summarise(sd_rating = sd(rating, na.rm = TRUE))                                        # standard deviation rating
+
+mean_nr_reviews <- df %>% group_by(device_type) %>%         
+  summarise(mean_nr_reviews = mean(nr_reviews, na.rm = TRUE))                            # mean nr reviews
+sd_nr_reviews <- df %>% group_by(device_type) %>% 
+  summarise(sd_nr_reviews = sd(nr_reviews, na.rm = TRUE))                                # standard deviation nr reviews
+
+                                                                                       
 ggplot(mean_prices, aes(x = device_type, y= mean_price)) + 
-  geom_bar(stat = "identity", fill = "indianred3", color = "black") + 
+  geom_bar(stat = "identity", fill = "indianred3", color = "black") +                    # mean prices per device type
   geom_text(aes(label = mean_price), vjust = 2, size = 4) +
   labs(x = "Device type", y = "Mean price", title  = "Mean prices per device type") 
 
+# statistics of device specifications
+???????????????????
+???????????????????
+???????????????????
 
 
                                               #####################
                                               #######Top  15#######
                                               #####################
 
-
-# Filter 15 most common brands for valuable analysis
+# Filter 15 most common brands for valuable price analysis
 tab <- table(df$brand)                        # calculate frequencies
 tab_s <- sort(tab)                            # sort
 top15 <- tail(names(tab_s), 15)               # extract 15 most frequent brands
 df_common <- subset(df, brand %in% top15)     # subset of data frame based on common brands
 
 # price distributions per category
-
 boxplot(df_common$price~df_common$brand,main="General price distribution by brand", 
         ylab="", xlab="price", ylim=c(100, 4000), horizontal = TRUE, las = 2)
 
